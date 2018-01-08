14d655a6d3269aa5061e712c6c207abc85dc1c84
MeePwn <maybewaityou@gmail.com>
2018-01-08 14:42:51 +0800

    first commit

---
+++ 3.6/webpack_server.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './index.jsx',
+  target: 'node',
+  externals: [ nodeExternals() ],
+  output: {
+    libraryTarget: 'commonjs2',
+    filename: 'bundle_server.js',
+    path: path.resolve(__dirname, './dist')
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: ['babel-loader'],
+        exclude: path.resolve(__dirname, 'node_modules')
+      },
+      {
+        test: /\.css$/,
+        use: ['ignore-loader']
+      }
+    ]
+  }
+};
