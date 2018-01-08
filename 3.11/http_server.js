28f9b5bee0ecf8b0a859ff054614f67641ccf678
MeePwn <maybewaityou@gmail.com>
2018-01-08 14:24:12 +0800

    first commit

--- 3.11/http_server.js
+++ 3.11/http_server.js
@@ -1,12 +1,5 @@
-/**
- * Created by MeePwn
- * https://github.com/maybewaityou
- *
- * description:
- *
- */
 const express = require('express');
-const { renderApp } = require('../dist/bundle_server');
+const { render } = require('./dist/bundle_server');
 const app = express();
 
 // 调用构建出的 bundle_server.js 中暴露出的渲染函数，再拼接下 HTML 模版，形成完整的 HTML 文件
@@ -17,8 +10,8 @@ app.get('/', function (req, res) {
   <meta charset="UTF-8">
 </head>
 <body>
-<div id="app">${renderApp()}</div>
-<!--导入 Webpack 输出的用于浏览器端渲染的 JS 文件-->
+<div id="app">${render()}</div>
+<!--导入 webpack 输出的用于浏览器端渲染的 JS 文件-->
 <script src="./dist/bundle_browser.js"></script>
 </body>
 </html>
