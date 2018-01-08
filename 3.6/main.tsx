6be17472865399ac182290abd8cbafd21a2f8baf
MeePwn <maybewaityou@gmail.com>
2018-01-08 10:24:24 +0800

    first commit

--- 3.6/main.tsx
+++ 3.6/main.tsx
@@ -10,10 +10,10 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 
-class Button extends React.Component {
+class Title extends React.Component {
   render() {
     return <h1>Hello, Webpack</h1>
   }
 }
 
-render(<Button />, window.document.getElementById('app'));
+render(<Title />, window.document.getElementById('app'));
